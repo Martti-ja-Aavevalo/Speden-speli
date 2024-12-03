8b51f423771f5aa461ea0612ba5ca9ba5bb4628d
Eero12 <eerooja18@gmail.com>
2024-12-03 10:07:31 +0200

    joko joulukuussa saa kuunnella joululauluja?

--- music/8bittinenmusasointi/8bittinenmusasointi.ino
+++ music/8bittinenmusasointi/8bittinenmusasointi.ino
@@ -2,35 +2,32 @@
 #include <Arduino_FreeRTOS.h>
 #include <Arduino.h>
 #include "pitches.cpp"
+#include "songCompiler.cpp"
 
-// define two tasks for Blink & AnalogRead
+
+
 void Buzzer( void *pvParameters );
 const int buzzer = 10;
-// the setup function runs once when you press reset or power the board
 void setup() {
   
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(buzzer, OUTPUT); 
   
-  while (!Serial) { // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-
+  while (!Serial) { 
   }
 
   xTaskCreate(
     Buzzer
-    ,  "Buzzer"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  "Buzzer"  
+    ,  128 
     ,  NULL
-    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  1  
     ,  NULL );
-
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
+
 }
 
 
@@ -42,6 +39,7 @@ void Buzzer(void *pvParameters)
   for (;;)
   {
     tone(buzzer, NOTE_A3);
+    Serial.print("a");
     delay(100);
     tone(buzzer, NOTE_B3);
     delay(100);
---
+++ music/8bittinenmusasointi/songCompiler.cpp
@@ -0,0 +1,11 @@
+#include "mainheader.h"  
+#include "Arduino.h"
+
+
+
+int song(int melody){
+
+  Serial.print(melody);
+
+
+}
